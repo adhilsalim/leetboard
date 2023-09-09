43e73539d1cb8231e705ceee7fc67f68232d5b78
Adhil Salim <bizadhil@gmail.com>
2023-09-09 09:16:35 +0530

    rm logs

--- set leaderboard/setLeaderboard.js
+++ set leaderboard/setLeaderboard.js
@@ -8,16 +8,8 @@ function setLeaderboard() {
 
 function sortBoard() {
   for (let i = 1; i < leaderboardSheetData.length + 1; i++) {
-    console.log("running");
     for (let j = 1; j < leaderboardSheetData.length - i - 1 + 1; j++) {
-      console.log(
-        "checking whether",
-        leaderboardSheetData[j][4],
-        "is less than",
-        leaderboardSheetData[j + 1][4]
-      );
       if (leaderboardSheetData[j][4] < leaderboardSheetData[j + 1][4]) {
-        console.log("yes true");
         swapRows(j, j + 1);
       }
     }
@@ -25,7 +17,6 @@ function sortBoard() {
 }
 
 function swapRows(rowOneIndex, rowTwoIndex) {
-  console.log("row index 1: ", rowOneIndex, "row index 2: ", rowTwoIndex);
   if (
     rowOneIndex < 1 ||
     rowTwoIndex < 1 ||
@@ -33,18 +24,11 @@ function swapRows(rowOneIndex, rowTwoIndex) {
     rowTwoIndex > leaderboardSheetData.length
   ) {
     Logger.log("Invalid row numbers. Rows to swap are out of range.");
-    console.log("invalid row index");
     return;
   }
-  console.log("Before swapping");
-  console.log(leaderboardSheetData[rowOneIndex]);
-  console.log(leaderboardSheetData[rowTwoIndex]);
   let tempRow = leaderboardSheetData[rowOneIndex];
   leaderboardSheetData[rowOneIndex] = leaderboardSheetData[rowTwoIndex];
   leaderboardSheetData[rowTwoIndex] = tempRow;
-  console.log("After swapping");
-  console.log(leaderboardSheetData[rowOneIndex]);
-  console.log(leaderboardSheetData[rowTwoIndex]);
 
   leaderboardSheet
     .getRange(1, 1, leaderboardSheetData.length, leaderboardSheetData[0].length)
