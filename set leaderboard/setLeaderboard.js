3a3de0c29b8ba7744c7cce6af5f98ca43ab964b9
Adhil Salim <bizadhil@gmail.com>
2023-09-09 08:51:01 +0530

    update the value in sheet

--- set leaderboard/setLeaderboard.js
+++ set leaderboard/setLeaderboard.js
@@ -1,11 +1,10 @@
 function setLeaderboard() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet();
-  var leaderboard = sheet
-    .getSheetByName("leaderboard")
-    .getDataRange()
-    .getValues();
+  const sheet = SpreadsheetApp.getActiveSpreadsheet();
+  const leaderboardSheet = sheet.getSheetByName("leaderboard");
+  let leaderboardSheetData = leaderboardSheet.getDataRange().getValues();
 
-  for (let i = 1; i < leaderboard.length; i++) {
-    console.log(fetchLeetCodeData(leaderboard[i][3]));
+  for (let i = 1; i < leaderboardSheetData.length; i++) {
+    leaderboardSheetData[i][4] = fetchLeetCodeData(leaderboardSheetData[i][3]);
+    leaderboardSheet.getRange(i + 1, 5).setValue(leaderboardSheetData[i][4]);
   }
 }
